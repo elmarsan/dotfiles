5afa502ee032714c363b82c8ef6b3e84f4fbe8b3
elmarsan <mselias97@gmail.com>
2023-11-17 20:25:55 +0100

    nvim: modify telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,19 +1,13 @@
 local builtin = require('telescope.builtin')
 
--- Find files
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- Find in git files 
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
--- Find by word globally
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
--- Find help
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- Find in buffers
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
--- Find in current working directory
-vim.keymap.set('n', '<leader>fd', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fd', builtin.live_grep, {})
 
 local oxocarbon = require("oxocarbon")
 
