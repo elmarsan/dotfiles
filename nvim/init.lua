f20dd9fec4b80e677fdb97a75c96b8a46bf45a3c
elmarsan <mselias97@gmail.com>
2025-02-19 09:22:21 +0100

    Add lsp rename keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -224,6 +224,7 @@ vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
 vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation)
 vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 
 vim.keymap.set('n', "<leader>tt", ":NvimTreeToggle<CR>")
 vim.keymap.set('n', "<leader>tf", ":NvimTreeFocus<CR>")
